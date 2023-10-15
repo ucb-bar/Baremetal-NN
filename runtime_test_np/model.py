94ae5612edd38486518d3779680ceb1ff2796ef0
-T.K.- <t_k_233@outlook.com>
2023-10-14 18:41:31 -0700

    FIX: fix numpy example

--- runtime_test_np/model.py
+++ runtime_test_np/model.py
@@ -8,9 +8,9 @@ def forward(input):
     # Input
     input_out = input
     # Linear
-    i2h_out = nn_linear(input_out, i2h_weight_T, i2h_bias)
+    i2h_out = nn_linear(input_out, i2h_weight_transposed, i2h_bias)
     # Linear
-    h2o_out = nn_linear(i2h_out, h2o_weight_T, h2o_bias)
+    h2o_out = nn_linear(i2h_out, h2o_weight_transposed, h2o_bias)
     # Log Softmax
     softmax_out = nn_logsoftmax(h2o_out)
     return softmax_out, i2h_out
