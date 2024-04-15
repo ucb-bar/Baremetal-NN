06a75cc4d24b2c7429044d394909a003fc615123
-T.K.- <t_k_233@outlook.com>
2024-04-14 20:56:18 -0700

    REFACTOR: clean old files

--- runtime_test_np/nn.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def nn_linear(input, weight_T, bias):
-    return np.matmul(input, weight_T) + bias
-
-def nn_logsoftmax(input):
-    return np.log(np.exp(input) / np.sum(np.exp(input), axis=1))
