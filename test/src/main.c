ed9ccb9a159546b786043b28d7ceeaaa6c8e3685
-T.K.- <t_k_233@outlook.com>
2024-06-05 16:07:30 -0700

    ADD: update test

--- test/src/main.c
+++ test/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "nn.h"
 #include "riscv_vector.h"
@@ -31,27 +32,6 @@ void NN_random(Tensor *t) {
   }
 }
 
-void NN_matmul_F32_RVV(Tensor *out, Tensor *a, Tensor *b) {
-  size_t vlmax = __riscv_vsetvlmax_e32m1();
-  vfloat32m1_t vec_zero = __riscv_vfmv_v_f_f32m1(0, vlmax);
-  for (int i = 0; i < a->shape[0]; i += 1) {
-    for (int j = 0; j < b->shape[1]; j += 1) {
-      float *ptr_a = (float *)a->data + i * a->shape[1];
-      float *ptr_b = (float *)b->data + j;
-      vfloat32m1_t vec_s = __riscv_vfmv_v_f_f32m1(0, vlmax);
-      size_t vl = 0;
-      for (int k = a->shape[1]; k > 0; k -= vl, ptr_a += vl, ptr_b += vl) {
-        vl = __riscv_vsetvl_e32m1(k);
-        vfloat32m1_t vec_a = __riscv_vlse32_v_f32m1(ptr_a, sizeof(float), vl);
-        vfloat32m1_t vec_b = __riscv_vlse32_v_f32m1(ptr_b, b->shape[1]*sizeof(float), vl);
-        vec_s = __riscv_vfmacc_vv_f32m1(vec_s, vec_a, vec_b, vl);
-      }
-      vfloat32m1_t vec_sum = __riscv_vfredusum_vs_f32m1_f32m1(vec_s, vec_zero, vlmax);
-      float sum = __riscv_vfmv_f_s_f32m1_f32(vec_sum);
-      ((float *)out->data)[i * out->shape[1] + j] = sum;
-    }
-  }  
-}
 
 #define N  4
 #define M  6
