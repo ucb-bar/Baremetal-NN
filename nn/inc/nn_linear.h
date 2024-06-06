d2ed3a71de4604a668743376ef7d679c2b12922d
-T.K.- <t_k_233@outlook.com>
2024-06-05 21:49:38 -0700

    ADD: add correct linear impl

--- nn/inc/nn_linear.h
+++ nn/inc/nn_linear.h
@@ -3,15 +3,23 @@
 
 #include <assert.h>
 
-#include "nn_types.h"
+#include "nn_tensor.h"
 #include "nn_add.h"
 #include "nn_matmul.h"
 
 
 /**
- * Applies a linear transformation to the incoming data: y = x @ w.T + b
+ * Applies a linear transformation to the incoming data
  * 
+ * y = x @ w.T + b
+ * 
+ * @param y Output tensor of shape (1, out_features)
+ * @param x Input tensor of shape (1, in_features)
+ * @param w Weight tensor of shape (out_features, in_features)
+ * @param b Bias tensor of shape (1, out_features)
  */
 void NN_linear_F32(Tensor *y, Tensor *x, Tensor *w, Tensor *b);
+
+void NN_linear_F32_RVV(Tensor *y, Tensor *x, Tensor *w, Tensor *b);
 
 #endif // __NN_LINEAR_H
