91e83af9bacd110027569b2b41a7195e9b165ed5
-T.K.- <t_k_233@outlook.com>
2024-06-20 11:45:40 -0700

    ADD: add conv2d with groups

--- nn/src/nn_conv2d.c
+++ nn/src/nn_conv2d.c
@@ -5,6 +5,84 @@
   #include "gemmini/gemmini.h"
 #endif
 
+
+/**
+ * Converts a tensor from NCHW (PyTorch) to NHWC (Channel-last) format.
+ * 
+ * @param out: the output tensor of shape (batch_size, height, width, channels)
+ * @param in: the input tensor of shape (batch_size, channels, height, width)
+ */
+void NN_NCHWToNHWC(Tensor *out, Tensor *in) {
+  assert(in->ndim == 4);
+  assert(out->ndim == 4);
+  assert(in->dtype == DTYPE_F32);
+  assert(out->dtype == DTYPE_F32);
+  assert(out->shape[0] == in->shape[0]); // batch size must match
+  assert(out->shape[1] == in->shape[2]); // height must match
+  assert(out->shape[2] == in->shape[3]); // width must match
+  assert(out->shape[3] == in->shape[1]); // channels must match
+
+  size_t batch_size = in->shape[0];
+  size_t height = in->shape[2];
+  size_t width = in->shape[3];
+  size_t channels = in->shape[1];
+
+  for (size_t n = 0; n < batch_size; n += 1) {
+    for (size_t c = 0; c < channels; c += 1) {
+      for (size_t h = 0; h < height; h += 1) {
+        for (size_t w = 0; w < width; w += 1) {
+          size_t nchw_index = n * channels * height * width + c * height * width + h * width + w;
+          size_t nhwc_index = n * height * width * channels + h * width * channels + w * channels + c;
+          ((float *)out->data)[nhwc_index] = ((float *)in->data)[nchw_index];
+        }
+      }
+    }
+  }
+}
+
+
+/**
+ * Converts a tensor from NHWC (Channel-last) to NCHW (PyTorch) format.
+ * 
+ * @param out: the output tensor of shape (batch_size, channels, height, width)
+ * @param in: the input tensor of shape (batch_size, height, width, channels)
+ */
+void NN_NHWCToNCHW(Tensor *out, Tensor *in) {
+  assert(in->ndim == 4);
+  assert(out->ndim == 4);
+  assert(in->dtype == DTYPE_F32);
+  assert(out->dtype == DTYPE_F32);
+  assert(out->shape[0] == in->shape[0]); // batch size must match
+  assert(out->shape[1] == in->shape[3]); // channels must match
+  assert(out->shape[2] == in->shape[1]); // height must match
+  assert(out->shape[3] == in->shape[2]); // width must match
+
+  size_t batch_size = in->shape[0];
+  size_t height = in->shape[1];
+  size_t width = in->shape[2];
+  size_t channels = in->shape[3];
+
+  for (size_t n = 0; n < batch_size; n += 1) {
+    for (size_t c = 0; c < channels; c += 1) {
+      for (size_t h = 0; h < height; h += 1) {
+        for (size_t w = 0; w < width; w += 1) {
+          size_t nhwc_index = n * height * width * channels + h * width * channels + w * channels + c;
+          size_t nchw_index = n * channels * height * width + c * height * width + h * width + w;
+          ((float *)out->data)[nchw_index] = ((float *)in->data)[nhwc_index];
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Applies a 2D convolution over an input signal composed of several input planes.
+ * 
+ * @param out: the output tensor of shape (batch_size, channels_in, height, width)
+ * @param in: the input tensor of shape (batch_size, channels_out, height, width)
+ * @param weight: the learnable weights of the module of shape (kernel_height, kernel_width, channels_in, channels_out)
+ * @param bias: the learnable bias of the module of shape (channels_out), or NULL if no bias is applied
+ */
 void NN_Conv2d(
   Tensor *out, Tensor *in,
   Tensor *weight, Tensor *bias,
@@ -18,20 +96,16 @@ void NN_Conv2d(
     assert(bias->ndim == 1);
     assert(bias->dtype == DTYPE_F32);
   }
-  assert(out->shape[0] == in->shape[0]);
-  assert(out->shape[3] == weight->shape[3]);
-  assert(in->shape[3] == weight->shape[2] * groups);
-  assert(out->shape[1] == (in->shape[1] + 2 * padding[0] - dilation[0] * (weight->shape[0] - 1) - 1) / stride[0] + 1);
-  assert(out->shape[2] == (in->shape[2] + 2 * padding[1] - dilation[1] * (weight->shape[1] - 1) - 1) / stride[1] + 1);
-  assert(groups > 0);
-  assert(in->shape[3] % groups == 0);
-  assert(out->shape[3] % groups == 0);
 
   size_t batch_size = in->shape[0];
-  size_t out_channels = out->shape[3];
-  size_t in_channels = in->shape[3];
   size_t in_height = in->shape[1];
   size_t in_width = in->shape[2];
+  size_t in_channels = in->shape[3];
+  
+  size_t out_height = out->shape[1];
+  size_t out_width = out->shape[2];
+  size_t out_channels = out->shape[3];
+
   size_t kernel_height = weight->shape[0];
   size_t kernel_width = weight->shape[1];
   size_t stride_height = stride[0];
@@ -41,28 +115,97 @@ void NN_Conv2d(
   size_t dilation_height = dilation[0];
   size_t dilation_width = dilation[1];
 
-  size_t out_height = (in_height + 2 * padding_height - kernel_height) / stride_height + 1;
-  size_t out_width = (in_width + 2 * padding_width - kernel_width) / stride_width + 1;
+  assert(out->shape[0] == batch_size);
+  assert(weight->shape[3] == out_channels);
+  assert(weight->shape[2] * groups == in_channels);
+  assert(out_height == (in_height + 2 * padding_height - dilation_height * (kernel_height - 1) - 1) / stride_height + 1);
+  assert(out_width == (in_width + 2 * padding_width - dilation_width * (kernel_width - 1) - 1) / stride_width + 1);
+  assert(groups > 0);
+  assert(in_channels % groups == 0);
+  assert(out_channels % groups == 0);
+
 
   // Initialize output tensor to zeros
   memset(out->data, 0, batch_size * out_height * out_width * out_channels * sizeof(float));
 
   #ifdef GEMMINI
-    // tiled_conv_auto(
-    //     batch_size, in_height, in_width, in_channels,
-    //     out_channels, out_height, out_width,
-    //     stride_height, dilation_height, 1, padding_height, kernel_height, 
-    //     0, 0, 0, 0, 0,
+    if (groups == 1) {
+      tiled_conv_auto(
+          batch_size, in_height, in_width, in_channels,
+          out_channels, out_height, out_width,
+          stride_height, dilation_height, 1, padding_height, kernel_height, 
+          0, 0, 0, 0, 0,
 
-    //     in->data,
-    //     weight->data,
-    //     bias->data,
-    //     out->data,
+          in->data,
+          weight->data,
+          bias->data,
+          out->data,
 
-    //     NO_ACTIVATION, ACC_SCALE_IDENTITY,
-    //     0, 0, 0,
+          NO_ACTIVATION, ACC_SCALE_IDENTITY,
+          0, 0, 0,
 
-    //     WS);
+          WS);
+    }
+    else {
+      assert(weight->shape[2] == 1);
+
+      Tensor *in_nchw = NN_tensor(4, (size_t[]){batch_size, in_channels, in_height, in_width}, DTYPE_F32, NULL);
+      Tensor *out_nchw = NN_tensor(4, (size_t[]){batch_size, out_channels, out_height, out_width}, DTYPE_F32, NULL);
+      
+      Tensor *weight_1hwc = NN_tensor(4, (size_t[]){1, kernel_height, kernel_width, out_channels}, DTYPE_F32, weight->data);
+      Tensor *weight_1chw = NN_tensor(4, (size_t[]){1, out_channels, kernel_height, kernel_width}, DTYPE_F32, NULL);
+
+      // printf("in:\t");
+      // NN_printShape(in);
+      // printf("\n");
+      // NN_printf(in);
+      // printf("in_nchw:\t");
+      // NN_printShape(in_nchw);
+      // printf("\n");
+      // NN_printf(in_nchw);
+
+      
+      // printf("weight_1hwc:\t");
+      // NN_printShape(weight_1hwc);
+      // printf("\n");
+      // NN_printf(weight_1hwc);
+      // printf("weight_1chw:\t");
+      // NN_printShape(weight_1chw);
+      // printf("\n");
+      // NN_printf(weight_1chw);
+
+      NN_NHWCToNCHW(in_nchw, in);
+      NN_NHWCToNCHW(weight_1chw, weight_1hwc);
+
+
+      
+
+      for (size_t g = 0; g < groups; g += 1) {
+        tiled_conv_auto(
+          batch_size, in_height, in_width, 1,
+          1, out_height, out_width,
+          stride_height, dilation_height, 1, padding_height, kernel_height, 
+          0, 0, 0, 0, 0,
+
+          ((float *)in_nchw->data) + g * in_height * in_width,
+          ((float *)weight_1chw->data) + g * kernel_height * kernel_width,
+          ((float *)bias->data) + g,
+          ((float *)out_nchw->data) + g * out_height * out_width,
+
+          NO_ACTIVATION, ACC_SCALE_IDENTITY,
+          0, 0, 0,
+
+          WS);
+
+          // printf("channel group %d\n", g);
+          // NN_printf(out_nchw);
+          // printf("\n\n");
+      }
+
+      NN_NCHWToNHWC(out, out_nchw);
+    }
+
+
   #else
     for (size_t n = 0; n < batch_size; n += 1) {
       for (size_t g = 0; g < groups; g += 1) {
