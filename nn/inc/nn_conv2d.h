f916fda63615eeab85bcd6f6edd1762ae7ba6bd2
-T.K.- <t_k_233@outlook.com>
2024-06-20 12:52:24 -0700

    ADD: proper format header

--- nn/inc/nn_conv2d.h
+++ nn/inc/nn_conv2d.h
@@ -6,18 +6,32 @@
 
 #include "nn_tensor.h"
 
+/**
+ * Converts a tensor from NCHW (PyTorch) to NHWC (Channel-last) format.
+ * 
+ * @param out: the output tensor of shape (batch_size, height, width, channels)
+ * @param in: the input tensor of shape (batch_size, channels, height, width)
+ */
+void NN_NCHWToNHWC(Tensor *out, Tensor *in);
+
+/**
+ * Converts a tensor from NHWC (Channel-last) to NCHW (PyTorch) format.
+ * 
+ * @param out: the output tensor of shape (batch_size, channels, height, width)
+ * @param in: the input tensor of shape (batch_size, height, width, channels)
+ */
+void NN_NHWCToNCHW(Tensor *out, Tensor *in);
 
 /**
  * Applies a 2D convolution over an input signal composed of several input planes.
  * 
  * @param out: the output tensor of shape (batch_size, channels_in, height, width)
  * @param in: the input tensor of shape (batch_size, channels_out, height, width)
- * @param weight: the learnable weights of the module of shape (channels_out, channels_in, kernel_size[0], kernel_size[1])
+ * @param weight: the learnable weights of the module of shape (kernel_height, kernel_width, channels_in, channels_out)
  * @param bias: the learnable bias of the module of shape (channels_out), or NULL if no bias is applied
- * @param in_channels: number of channels in the input tensor
- * @param out_channels: number of channels produced by the convolution
  * @param stride: stride for the cross-correlation
  * @param padding: the amount of padding applied to the input
+ * @param dilation: the spacing between kernel elements
  * @param groups: number of blocked connections from input channels to output channels
  */
 void NN_Conv2d(
--- nn/src/nn_conv2d.c
+++ nn/src/nn_conv2d.c
@@ -6,12 +6,6 @@
 #endif
 
 
-/**
- * Converts a tensor from NCHW (PyTorch) to NHWC (Channel-last) format.
- * 
- * @param out: the output tensor of shape (batch_size, height, width, channels)
- * @param in: the input tensor of shape (batch_size, channels, height, width)
- */
 void NN_NCHWToNHWC(Tensor *out, Tensor *in) {
   assert(in->ndim == 4);
   assert(out->ndim == 4);
@@ -40,13 +34,6 @@ void NN_NCHWToNHWC(Tensor *out, Tensor *in) {
   }
 }
 
-
-/**
- * Converts a tensor from NHWC (Channel-last) to NCHW (PyTorch) format.
- * 
- * @param out: the output tensor of shape (batch_size, channels, height, width)
- * @param in: the input tensor of shape (batch_size, height, width, channels)
- */
 void NN_NHWCToNCHW(Tensor *out, Tensor *in) {
   assert(in->ndim == 4);
   assert(out->ndim == 4);
@@ -75,14 +62,6 @@ void NN_NHWCToNCHW(Tensor *out, Tensor *in) {
   }
 }
 
-/**
- * Applies a 2D convolution over an input signal composed of several input planes.
- * 
- * @param out: the output tensor of shape (batch_size, channels_in, height, width)
- * @param in: the input tensor of shape (batch_size, channels_out, height, width)
- * @param weight: the learnable weights of the module of shape (kernel_height, kernel_width, channels_in, channels_out)
- * @param bias: the learnable bias of the module of shape (channels_out), or NULL if no bias is applied
- */
 void NN_Conv2d(
   Tensor *out, Tensor *in,
   Tensor *weight, Tensor *bias,
