e79801be5a27eb224da6f67b8e5e73b620e3a899
-T.K.- <t_k_233@outlook.com>
2024-07-09 14:22:39 -0700

    ADD: add fp16 matmul

--- nn/inc/ops/dot.h
+++ nn/inc/ops/dot.h
@@ -7,6 +7,41 @@
   #include <riscv_vector.h>
 #endif
 
+#include "nn_float16.h"
+
+
+static inline void NN__dot_F16(size_t n, float16_t *s, float16_t *x, float16_t *y) {
+  float16_t sum = 0.0;
+  
+  #ifdef RVV
+    size_t vlmax = __riscv_vsetvlmax_e16m1();
+
+    vfloat16m1_t vec_zero = __riscv_vfmv_v_f_f16m1(0, vlmax);
+    vfloat16m1_t vec_s = __riscv_vfmv_v_f_f16m1(0, vlmax);
+
+    while (n > 0) {
+      size_t vl = __riscv_vsetvl_e16m1(n);
+      vfloat16m1_t vec_x = __riscv_vle16_v_f16m1(x, vl);
+      vfloat16m1_t vec_y = __riscv_vle16_v_f16m1(y, vl);
+      vec_s = __riscv_vfmacc_vv_f16m1(vec_s, vec_x, vec_y, vl);
+        
+      x += vl;
+      y += vl;
+      n -= vl;
+    }
+    vec_s = __riscv_vfredusum_vs_f16m1_f16m1(vec_s, vec_zero, vlmax);
+    sum = __riscv_vfmv_f_s_f16m1_f16(vec_s);
+  #else
+    float sum_f32 = 0;
+    for (size_t i = 0; i < n; i += 1) {
+      sum_f32 += NN_halfToFloat(x[i]) * NN_halfToFloat(y[i]);
+    }
+    sum = NN_floatToHalf(sum_f32);
+  #endif
+
+  *s = sum;
+}
+
 static inline void NN__dot_F32(size_t n, float *s, float *x, float *y) {
   float sum = 0.0;
   
--- nn/src/nn_matmul.c
+++ nn/src/nn_matmul.c
@@ -24,12 +24,56 @@ void NN_matmul(Tensor *out, Tensor *a, Tensor *b) {
     }
     return;
   }
+  if (a->dtype == DTYPE_F16 && b->dtype == DTYPE_F16 && out->dtype == DTYPE_F16) {
+    // currently only support 2D matrix multiplication
+    assert(a->ndim == 2);
+    assert(b->ndim == 2);
+    assert(a->dtype == DTYPE_F16);
+    assert(b->dtype == DTYPE_F16);
+    assert(out->dtype == DTYPE_F16);
+    assert(a->shape[1] == b->shape[0]);
+    assert(out->shape[0] == a->shape[0]);
+    assert(out->shape[1] == b->shape[1]);
+
+    for (size_t i = 0; i < out->shape[0]; i += 1) {
+      for (size_t j = 0; j < out->shape[1]; j += 1) {
+        float sum = 0;
+        for (size_t k = 0; k < a->shape[1]; k += 1) {
+          sum += NN_halfToFloat(((float16_t *)a->data)[i * a->shape[1] + k]) * NN_halfToFloat(((float16_t *)b->data)[k * b->shape[1] + j]);
+        }
+        ((float16_t *)out->data)[i * out->shape[1] + j] = NN_floatToHalf(sum);
+      }
+    }
+    return;
+  }
   printf("Unsupported operation: %s = %s @ %s\n", 
     NN_getDataTypeName(out->dtype), NN_getDataTypeName(a->dtype), NN_getDataTypeName(b->dtype)
   );
 }
 
 void NN_matmulT(Tensor *out, Tensor *a, Tensor *b) {
+  if (a->dtype == DTYPE_F16 && b->dtype == DTYPE_F16 && out->dtype == DTYPE_F16) {
+    // currently only support 2D matrix multiplication
+    assert(a->ndim == 2);
+    assert(b->ndim == 2);
+    assert(a->dtype == DTYPE_F16);
+    assert(b->dtype == DTYPE_F16);
+    assert(out->dtype == DTYPE_F16);
+    assert(a->shape[1] == b->shape[1]);
+    assert(out->shape[0] == a->shape[0]);
+    assert(out->shape[1] == b->shape[0]);
+
+    for (size_t i = 0; i < out->shape[0]; i += 1) {
+      for (size_t j = 0; j < out->shape[1]; j += 1) {
+        NN__dot_F16(a->shape[1], 
+          (float16_t *)out->data + i * out->shape[1] + j, 
+          (float16_t *)a->data + i * a->shape[1], 
+          (float16_t *)b->data + j * b->shape[1]
+          );
+      }
+    }
+    return;
+  }
   if (a->dtype == DTYPE_F32 && b->dtype == DTYPE_F32 && out->dtype == DTYPE_F32) {
     // currently only support 2D matrix multiplication
     assert(a->ndim == 2);
