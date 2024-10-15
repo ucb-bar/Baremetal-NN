210603372ea8691df7a174be99791084fca85b74
-T.K.- <t_k_233@outlook.com>
2024-10-15 14:51:15 -0700

    REFACTOR: minor adjustment

--- src/cpu/mm.c
+++ src/cpu/mm.c
@@ -9,10 +9,10 @@ __attribute__((weak)) void nn_mm_f16(Tensor2D_F16 *y, const Tensor2D_F16 *x1, co
   const size_t in_features = x1->shape[1];
   const size_t out_features = x2->shape[0];
 
-  for (size_t i = 0; i < batch_size; i++) {
-    for (size_t j = 0; j < out_features; j++) {
+  for (size_t i = 0; i < batch_size; i += 1) {
+    for (size_t j = 0; j < out_features; j += 1) {
       float sum = 0.f;
-      for (size_t k = 0; k < in_features; k++) {
+      for (size_t k = 0; k < in_features; k += 1) {
         sum += as_f32(x1->data[i * in_features + k]) * as_f32(x2->data[j * in_features + k]);
       }
       y->data[i * out_features + j] = as_f16(sum);
@@ -28,10 +28,10 @@ __attribute__((weak)) void nn_mm_f32(Tensor2D_F32 *y, const Tensor2D_F32 *x1, co
   const size_t in_features = x1->shape[1];
   const size_t out_features = x2->shape[0];
 
-  for (size_t i = 0; i < batch_size; i++) {
-    for (size_t j = 0; j < out_features; j++) {
+  for (size_t i = 0; i < batch_size; i += 1) {
+    for (size_t j = 0; j < out_features; j += 1) {
       float sum = 0.f;
-      for (size_t k = 0; k < in_features; k++) {
+      for (size_t k = 0; k < in_features; k += 1) {
         sum += x1->data[i * in_features + k] * x2->data[j * in_features + k];
       }
       y->data[i * out_features + j] = sum;
